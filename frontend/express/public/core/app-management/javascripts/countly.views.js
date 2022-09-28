cbba47e9956c19e62e47b00be4b480178ba9f8fa
Pınar Genç <gncpin@hotmail.com>
2022-09-28 16:37:21 +0300

    fix

--- frontend/express/public/core/app-management/javascripts/countly.views.js
+++ frontend/express/public/core/app-management/javascripts/countly.views.js
@@ -293,9 +293,9 @@
                 }
             },
             handleUploadSuccess: function() {
+                this.showFileList = true;
                 this.app_icon["background-image"] = 'url("appimages/' + this.selectedApp + '.png?' + Date.now() + '")';
                 countlyGlobal.apps[this.selectedApp].image = "appimages/" + this.selectedApp + '.png?' + Date.now();
-                this.showFileList = true;
             },
             loadComponents: function() {
                 var cc = countlyVue.container.dataMixin({
